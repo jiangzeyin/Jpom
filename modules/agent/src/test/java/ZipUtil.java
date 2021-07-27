e25f3d383318633d0eee9d78b609352ad14084a0
bwcx_jzy <bwcx_jzy@163.com>
2021-07-27 22:32:11 +0800

    compression

--- modules/agent/src/test/java/ZipUtil.java
+++ modules/agent/src/test/java/ZipUtil.java
@@ -1,14 +1,31 @@
+import cn.hutool.core.io.CharsetDetector;
 import io.jpom.util.CompressionFileUtil;
 
 import java.io.File;
-import java.util.List;
+import java.io.FileInputStream;
 
 public class ZipUtil {
 
-    private static final int BUFFER_SIZE = 1024 * 100;
+	private static final int BUFFER_SIZE = 1024 * 100;
 
-    public static void main(String[] args) throws Exception {
-        List<String> list = CompressionFileUtil.unCompress(new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.bz2"), new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.gz11\\"));
-        System.out.println(list);
-    }
+	public static void main(String[] args) throws Exception {
+
+		File file = new File("D:\\SystemDir\\桌面\\jpom-2.5.0.tar.bz2");
+		File file1 = new File("D:\\SystemDir\\桌面\\ttt\\");
+
+
+		//	CompressionFileUtil.unCompress(file, file1);
+		file = new File("D:\\SystemDir\\桌面\\jpom-2.5.0.tar.gz");
+
+
+		System.out.println(CharsetDetector.detect(file));
+
+		System.out.println(CharsetDetector.detect(new FileInputStream(file)));
+
+
+		System.out.println(CharsetDetector.detect(new File("D:\\SystemDir\\桌面\\api.conf")));
+//		System.out.println(CharsetUtil.charset(new CharsetDetector().detectChineseCharset(file)));
+
+		CompressionFileUtil.unCompress(file, file1);
+	}
 }
